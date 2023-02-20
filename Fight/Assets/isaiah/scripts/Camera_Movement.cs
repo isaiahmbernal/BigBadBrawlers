aad200e6ae2d4b9c6d97f79fea22462dc53836da
isaiahmbernal <isaiahmbernal@gmail.com>
2023-02-19 21:02:06 -0600

    ✨ Camera Movement

---
+++ Fight/Assets/isaiah/scripts/Camera_Movement.cs
@@ -0,0 +1,90 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera_Movement : MonoBehaviour
+{
+
+  public GameManager GameManager;
+  public Camera mainCamera;
+  public float maxXPos;
+
+  void Awake()
+  {
+    
+    mainCamera = Camera.main;
+    GameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+  }
+
+  void FixedUpdate()
+  {
+    Distance();
+    Position();
+  }
+
+  void Position()
+  {
+    float totalX = 0f;
+    float totalY = 0f;
+    foreach (GameObject player in GameManager.playerList)
+    {
+      totalX += player.transform.position.x;
+      totalY += player.transform.position.y;
+    }
+    float centerX = totalX / GameManager.playerList.Count;
+    float centerY = totalY / GameManager.playerList.Count;
+
+    if (centerX < -4f)
+    {
+      centerX = -4f;
+    }
+    else if (centerX > 4f)
+    {
+      centerX = 4f;
+    }
+
+    if (centerY < -3f)
+    {
+      centerY = -3f;
+    }
+    else if (centerY > 2f)
+    {
+      centerY = 2f;
+    }
+
+    mainCamera.transform.position = new Vector3(centerX, centerY, -10);
+  }
+
+  void Distance()
+  {
+    float currGreatest = 0;
+    for (int i = 0; i < GameManager.playerList.Count - 1; i += 1)
+    {
+      // Debug.Log("i: " + i);
+      for (int j = i + 1; j < GameManager.playerList.Count; j += 1)
+      {
+        // Debug.Log("j: " + j);
+        float currDist = Vector3.Distance(GameManager.playerList[i].transform.position, GameManager.playerList[j].transform.position);
+        // Debug.Log("i: " + i + " | j: " + j);
+        Debug.Log("Distance " + currDist + " between " + GameManager.playerList[i].name + " and " + GameManager.playerList[j].name);
+        if (currDist > currGreatest)
+        {
+          currGreatest = currDist;
+          Debug.Log("New Distance: " + currGreatest);
+        }
+      }
+    }
+    if (currGreatest > 5)
+    {
+      mainCamera.orthographicSize = 5;
+    }
+    else if (currGreatest < 2)
+    {
+      mainCamera.orthographicSize = 2;
+    }
+    else
+    {
+      mainCamera.orthographicSize = currGreatest;
+    }
+  }
+}
--- Fight/Assets/isaiah/scripts/GameManager.cs
+++ Fight/Assets/isaiah/scripts/GameManager.cs
@@ -8,9 +8,11 @@ public class GameManager : MonoBehaviour
 
   public int playerNum;
   public Object[] playerPrefabs;
+  public List<GameObject> playerList;
 
   void Awake()
   {
+    playerList = new List<GameObject>();
     playerNum = 4;
     playerPrefabs = Resources.LoadAll("prefabs", typeof(GameObject));
   }
@@ -19,7 +21,7 @@ public class GameManager : MonoBehaviour
   {
     for (int i = 0; i < playerNum; i += 1)
     {
-      Debug.Log("i: " + i);
+      // Debug.Log("i: " + i);
       // GameObject player = (GameObject)AssetDatabase.LoadAssetAtPath("Assets/isaiah/prefabs/Proto", typeof(GameObject));
       // GameObject player = Resources.Load("Proto.prefab");
       Object prefab = playerPrefabs[0];
@@ -29,25 +31,30 @@ public class GameManager : MonoBehaviour
         case 0:
           player = (GameObject)Instantiate(prefab, new Vector3(-4, -1, 0), Quaternion.identity);
           player.name = "Player_One";
+          playerList.Add(player);
           Debug.Log("Player One Spawned");
           break;
         case 1:
           player = (GameObject)Instantiate(prefab, new Vector3(4, -1, 0), Quaternion.identity);
           player.name = "Player_Two";
+          playerList.Add(player);
           Debug.Log("Player Two Spawned");
           break;
         case 2:
           player = (GameObject)Instantiate(prefab, new Vector3(-4, 0, 0), Quaternion.identity);
           player.name = "Player_Three";
+          playerList.Add(player);
           Debug.Log("Player Three Spawned");
           break;
         case 3:
           player = (GameObject)Instantiate(prefab, new Vector3(4, 0, 0), Quaternion.identity);
           player.name = "Player_Four";
+          playerList.Add(player);
           Debug.Log("Player Four Spawned");
           break;
       }
     }
+    // Debug.Log(playerList[0].name);
   }
 
   void Update()
