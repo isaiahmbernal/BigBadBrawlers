22c6f35426ca72ce46ffa74f32af8753eb4baa46
MaggieR1411 <70910623+MaggieR1411@users.noreply.github.com>
2023-03-03 13:26:36 -0600

    Menu: Getting Started
    Got started on the main and options menu systems.

---
+++ Fight/Assets/maggie/Script/Menu_Buttons.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu_Buttons : MonoBehaviour
+{
+    public bool reload;
+    public void PlayGame()
+    {
+        int index = SceneManager.GetActiveScene().buildIndex;
+        if (!reload)
+        {
+            index += 1;
+            if (index == SceneManager.sceneCountInBuildSettings) { index = 0; }
+        }
+        SceneManager.LoadScene(index);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("QUIT!");
+        Application.Quit();
+    }
+}
